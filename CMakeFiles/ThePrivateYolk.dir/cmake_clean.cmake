384e43112aa2a91f7155a1fc70dfaa9362c37ff4
hacktheegg <hacktheegg@protonmail.com>
2024-01-23 00:54:37 +1100

    Initial Commit

---
+++ CMakeFiles/ThePrivateYolk.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ThePrivateYolk.dir/ThePrivateYolk.cpp.o"
+  "CMakeFiles/ThePrivateYolk.dir/ThePrivateYolk.cpp.o.d"
+  "ThePrivateYolk"
+  "ThePrivateYolk.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ThePrivateYolk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
