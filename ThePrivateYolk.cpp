3b18847b08aabfb2448350a4bf3b09ea23280081
hacktheegg <>
2024-01-24 23:24:03 +1100

    added menu class to go with the menuOption class

--- ThePrivateYolk.cpp
+++ ThePrivateYolk.cpp
@@ -1,17 +1,46 @@
-using namespace std;
 #include <iostream>
+#include <vector>
+using namespace std;
+
 
 
 class menuOption {
   private:
-    bool toggleable;
-    string state;
+    bool toggleable = false;
+    string state = "undefined";
+    string name = "undefined";
+
   public:
-    void toggle() { }
-    void setState() { }
-    string getState() { return "Unfinished"; }
+    menuOption(bool var1, string var2, string var3) :
+      toggleable(var1), name(var2), state(var3) { }
+
+    void setState(string var) { state = var; }
+
+    void toggle() {
+      if (toggleable == false) {
+        cerr << endl << "attempted to toggle menuOption when toggleable is false" << endl;
+      } else {
+        if (state == "false") {
+          state = "true";
+        } else if (state == "true") {
+          state = "false";
+        }
+      }
+    }
+
+    string getState() { return state; }
+
+    string getName() { return name; }
 };
 
+class menu {
+  private:
+    vector<menuOption> menuOptions;
+  public:
+    void addMenuOption(const menuOption& var) {
+      menuOptions.push_back(var);
+    }
+};
 
 
 /*
@@ -25,11 +54,11 @@ class menuOption {
 */
 
 
-
 int main() {
-	cout << "---Welcome to the Private Yolk---" <<
-	endl << "An open news platform so peole can be updated" <<
-	endl << "on whats happening faster and easier" << endl;
-  menuOption menu;
-  cout << endl << menu.getState() << endl;
+  cout << "---Welcome to the Private Yolk---" <<
+  endl << "An open news platform so peole can be updated" <<
+  endl << "on whats happening faster and easier" << endl;
+
+  menuOption menu(false, "CurrentName: ", "Current State");
+  cout << menu.getName() << menu.getState() << endl;
 }
